3a35bf7f861d53622b4cf3b6c97af1b30d5b03ee
AmmarWebDev <ammarnasr2008@gmail.com>
2023-06-30 17:53:40 +0300

    Favicon added and other stuff.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Hello, this's Template-2!
+# Hello, this's Kasper-Template-2!
+
+## Frameworks used:
+###   -Font Awesome
+###   -Google Fonts
+###   -Normalize.css
--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
-var author = "Ammar Nasr";
+var author = "Ammar Nasr",
+  frontEnd = "Ammar Nasr",
+  backEnd = null,
+  UI_UX_Designer = "Ammar Nasr";
 
 console.log("Made by", author);
